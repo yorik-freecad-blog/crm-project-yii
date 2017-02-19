a2400beb3abf0840d6da81a892a753004467a7f5
mguo manila <mparajes@hotmail.com>
2017-02-19 10:37:55 +0800

    commit for first stage project

---
+++ frontend/views/site/lynda.php
@@ -0,0 +1,22 @@
+<?php
+
+$this->title = 'Lynda Home Page';
+?>
+
+<div class="site-index">
+	<div class="jumbotron">
+		<h1>This is Lyndas Personal Home Page</h1>
+	</div>
+	<div class="body-content">
+		<div class="row">
+			<div class="col-lg-120">
+				<h1>Just a few personal to show</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-120">
+				<h1>Another personal to show</h1>
+			</div>
+		</div>
+	</div>
+</div>
